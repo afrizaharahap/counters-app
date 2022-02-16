50bd1c57b46aca651402043753b43b8a284abd40
afrizaharahap <afriza.harahap@gmail.com>
2022-02-16 17:07:20 +0700

    removing local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = { value: this.props.value };
+  // state = { value: this.props.counter.value };
 
   // renderTags() {
   //   if (this.state.tags.length === 0) return <p>There are no tags !</p>;
@@ -12,11 +12,11 @@ class Counter extends Component {
   //     </ul>
   //   );
   // }
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
-    // Syntax diatas adalah cara untuk mengupdate state (this.setstate)
-    // dengan membawa seluruh properti yang dimiliki oleh state tersebut
-  };
+  // handleIncrement = (product) => {
+  //   this.setState({ value: this.state.value + 1 });
+  // Syntax diatas adalah cara untuk mengupdate state (this.setstate)
+  // dengan membawa seluruh properti yang dimiliki oleh state tersebut
+  // };
 
   render() {
     //    let classes = this.getBadgeClassess();
@@ -30,13 +30,13 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()} // cara passing event argument
+          onClick={() => this.props.onIncrement(this.props.counter)} // cara passing event argument
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           // Raising sebuah even ke parent component (counters.jsx)
           // melalui sebuah props onDelete
           className="btn btn-danger btn-sm m-2"
@@ -51,14 +51,14 @@ class Counter extends Component {
   getBadgeClassess() {
     // baris ini adalah hasil refactoring (encapsulation)
     let classes = "badge m-2 badge-";
-    classes += this.state.value == 0 ? "warning" : "primary";
+    classes += this.props.counter.value == 0 ? "warning" : "primary";
     return classes;
     // Jika nilai count = 0 maka badge akan berwarna kuning,
     // sebaliknya berwarna biru.
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
